3ecf3754c687159debcf19c34d1f3c7d898aca24
Daryna Panfilova <107915770+Daryna-P@users.noreply.github.com>
2023-05-22 00:00:12 +0300

    statistics created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,21 @@
 import user from 'path/to/user.json';
 import {Profile} from './Profile/Profile';
-
+import data from 'path/to/data.json';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
     <>
-        <Profile
+      <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       followers={user.stats.followers}
       views={user.stats.views}
-      likes={user.stats.likes} />  
+      likes={user.stats.likes} /> 
+      <Statistics title="Upload stats" stats={data} />
     </>
+        
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+import { getRandomHexColor } from './randomColor';
+
+export const Statistics = ({title, stats}) => {
+    return(
+        <section className={css.statistics}>
+            {title && <h2 className={css["title"]}>{title}</h2>}
+
+    <ul className={css.stat_list}>
+        {stats.map(stat => (
+                <li key={stat.id}
+                    className={css.item}
+                    style={{
+                        backgroundColor: getRandomHexColor()
+                    }}>
+                    <span className={css.label}>{stat.label}</span>
+                    <span className={css.percentage}>{stat.percentage}</span>
+                </li>
+            ))}   
+  </ul>
+</section>
+    );
+};
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired
+    })
+    )
+}
